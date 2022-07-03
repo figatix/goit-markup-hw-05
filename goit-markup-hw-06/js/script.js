00c9259be0049a9b8ac7554a29f6416d59ba79be
figatix <60873339+figatix@users.noreply.github.com>
2022-07-03 22:43:43 +0300

    Add files via upload

---
+++ goit-markup-hw-06/js/script.js
@@ -0,0 +1,60 @@
+
+const reserve = document.querySelector('#reserve');
+
+reserve.addEventListener('click', modalOn)
+
+function modalOn() {
+  console.log('click')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(555)
+
+
+// const item = document.querySelector("#ttt");
+// const cover = document.querySelector('#ccc')
+
+// item.addEventListener("mouseover", func, false);
+// item.addEventListener("mouseout", func1, false);
+
+// function func() {  // not needed since item is already global, 
+//    // I am assuming this is here just because it's sample code?
+//    // var item = document.getElementById("button"); 
+//    // item.setAttribute("style", "color:blue;")
+//    console.log(111)
+
+//    cover.setAttribute("style", "background-color:blue;")
+   
+//    // item.style.height = '100px';
+// }
+
+// function func1()
+// {  
+//    // item.setAttribute("style", "color:green;")
+
+
+
+
+// }
